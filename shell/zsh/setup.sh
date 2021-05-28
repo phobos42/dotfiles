8ed57ac64b696e9607165abfa6bb47ecc8bc0b65
phobos42 <garrettruffner42@gmail.com>
2021-05-28 19:38:32 -0400

    :see_no_evil:

--- shell/zsh/setup.sh
+++ shell/zsh/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -dr $HOME/.dotfilerepo/shell/zsh/oh-my-zsh
+mv -r $HOME/.oh-my-zsh/ $HOME/.dotfilerepo/shell/zsh/oh-my-zsh
